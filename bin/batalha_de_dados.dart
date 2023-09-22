ee3db7a4cb4aec1172bff39cddc2e7d7d85cad17
Thiago Manzi <thiago_manzi@hotmail.com>
2023-09-22 17:39:56 -0300

    feat(p1): revelando o vencedor das rodadas e da partida

--- bin/batalha_de_dados.dart
+++ bin/batalha_de_dados.dart
@@ -40,5 +40,27 @@ void main(List<String> arguments) {
           "O ${(i + 1).toString()}º dado do adversário foi: ${valorDado.toString()}");
     }
     print("O total de pontos do seu adversário foi: ${somaPC.toString()}\n");
+
+    if (somaUsuario > somaPC) {
+      print("Você venceu a ${(i + 1).toString()}º rodada");
+      vitoriasUsuario++;
+    } else if (somaUsuario < somaPC) {
+      print("O Adversário venceu a ${(i + 1).toString()}º rodada");
+      vitoriasPC++;
+    } else {
+      print("A ${(i + 1).toString()}º rodada empatou");
+    }
+
+    print("\nPlacar: Você $vitoriasUsuario X $vitoriasPC Adversário");
+  }
+
+  if (vitoriasUsuario > vitoriasPC) {
+    print("\nVocê venceu a partida!\n");
+    vitoriasUsuario++;
+  } else if (vitoriasUsuario < vitoriasPC) {
+    print("\nVocê perdeu!\n");
+    vitoriasPC++;
+  } else {
+    print("\nEmpate!\n");
   }
 }
